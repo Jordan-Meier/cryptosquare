b809fca76f4cb210813acb2f1269d825dacf4a70
Jordan Meier and Kevin Mattison <jbirdmeier@gmail.com>
2016-01-19 15:06:25 -0800

    Add functionality to remove spaces from text

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 var simplifyText = function (message) {
   message = message.toLowerCase("Hi");
+  message = message.replace(/\s/g, '');
   return message;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe("simplifyText", function () {
   it("makes text lower case", function () {
     expect(simplifyText("Hi")).to.equal("hi");
   });
+
+  it("removes spaces from text", function () {
+    expect(simplifyText(" ")).to.equal("");
+  });
 });
